268c0900e03a11fc2186c1b9e642184e047e22fa
Ajitesh <ajitesh@gmail.com>
2016-09-16 10:47:11 +0000

    first commit

---
+++ springboot/src/main/java/hello/HelloController.java
@@ -0,0 +1,23 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@SpringBootApplication
+public class HelloController {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(HelloController.class, args);
+    }
+
+}
